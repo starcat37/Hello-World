32fae342f8ca840bc35ffb2c9f83d59af374dc30
Suin Kim <78073229+starcat37@users.noreply.github.com>
2021-04-28 18:32:23 +0900

    Update print-Hello.py
    or 오류 수정

--- print-Hello.py
+++ print-Hello.py
@@ -6,12 +6,17 @@
 print("Supported country: South Korea, USA, Japan, China")
 country = input("Please enter your country. (English) : ")
 
-if country == "Korea" or "South Korea" or "Republic of Korea": print("안녕하세요!")
-elif country == "USA" or "America": print("Hello!")
-elif country == "Japan": print("こんにちは！")
-elif country == "China": print("你好！")
+if country == "South Korea": 
+    print("안녕하세요!")
+elif country == "USA": 
+    print("Hello!")
+elif country == "Japan": 
+    print("こんにちは！")
+elif country == "China": 
+    print("你好！")
 else: print("Unsupported country.")
 
+  #문자열 비교에서 or은 쓰면 안되는 것 같다...
   
   #국가명이 다양한 나라들이 있는데, 이런 데는 리스트로 관리하는 게 나으려나
   #utf 같은 거 신경 안 써도 되나
