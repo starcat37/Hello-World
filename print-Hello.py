d8158e52ca4b6daf0eee195db85f62904cef9aef
Suin Kim <78073229+starcat37@users.noreply.github.com>
2021-04-28 18:23:15 +0900

    Update print-Hello.py
    들여쓰기 오류 수정(아마도...)

--- print-Hello.py
+++ print-Hello.py
@@ -7,15 +7,15 @@ print("Supported country: South Korea, USA, Japan, China")
 country = input("Please enter your country. (English) : ")
 
 if country == "Korea" or "South Korea" or "Republic of Korea":
-  print("안녕하세요!")
+    print("안녕하세요!")
 elif country == "USA" or "America":
-  print("Hello!")
+    print("Hello!")
 elif country == "Japan":
-  print("こんにちは！")
+    print("こんにちは！")
 elif country == "China":
-  print("你好！")
+    print("你好！")
 else:
-  print("Unsupported country.")
+    print("Unsupported country.")
 
   
   #국가명이 다양한 나라들이 있는데, 이런 데는 리스트로 관리하는 게 나으려나
