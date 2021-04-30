0ce3be01c2e7c5ee128d0f87f9281130f2155318
Suin Kim <78073229+starcat37@users.noreply.github.com>
2021-05-01 03:13:35 +0900

    Datatype change & new option
    Change datatype into Dictionary & Users can enter their own data about 'Hello!' in their language.

--- print-Hello.py
+++ print-Hello.py
@@ -9,10 +9,19 @@ print(f.renderText("Hello World!"))
 
 #국가명을 input으로 받아 해당 국가의 언어 인사를 출력하는 프로그램
 
+dic_coun_lan = {'South Korea':"안녕하세요!", 'USA':"Hello!", 'Japan':'こんにちは！', 'China':'你好！'}
+
 print("Supported country: South Korea, USA, Japan, China")
 country = input("Please enter your country. (English) : ")
 
-if country == "South Korea": 
+if dic_coun_lan.get(country) == None:
+    b = input("There's no data about your country.\nPlease enter 'Hello!' in your country language.: ")
+    dic_coun_lan[country] = b #해당하는 국가가 없을 경우 이용자가 직접 해당 인사 데이터를 입력
+
+print(dic_coun_lan.get(country))
+#딕셔너리에서 key로 value 찾기: dic.get(a)
+
+"""if country == "South Korea": 
     print("안녕하세요!")
 elif country == "USA": 
     print("Hello!")
@@ -20,7 +29,7 @@ elif country == "Japan":
     print("こんにちは！")
 elif country == "China": 
     print("你好！")
-else: print("Unsupported country.")
+else: print("Unsupported country.")"""
 
   #문자열 비교에서 or은 쓰면 안되는 것 같다...
   
